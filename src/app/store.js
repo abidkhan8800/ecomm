3e89c4787a1b370b88676f2e76d32cc361672b33
Abid Khan <abid.khan@kivihealth.com>
2023-03-28 16:10:34 +0530

    added react notifications

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import productReducer from  '../features/products/productSlice';
 import cartReducer from '../features/cart/cartSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     product: productReducer,
     cart: cartReducer
   },
